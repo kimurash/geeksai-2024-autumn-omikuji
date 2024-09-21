91b07af9be9434d7f039782606d426233a9f0089
shunsei <kimshun1227@icloud.com>
2024-09-21 12:15:20 +0900

    Add omikuji spinning animation

--- script.js
+++ script.js
@@ -160,12 +160,50 @@ document.getElementById("omikujiButton").addEventListener("click", function () {
         }
     ];
 
-    // おみくじの内容をランダムに変更
-    const index = Math.floor(Math.random() * lectures.length);
-    // おみくじの内容を画面に表示
-    document.getElementById("omikujiTitle").textContent = lectures[index].title;
-    document.getElementById("omikujiDetails").textContent =
-        `${lectures[index].time}, ${lectures[index].company}, ホール: ${lectures[index].hall}`;
+    // ボタンを読み込む
+    const omikujiButton = document.getElementById("omikujiButton");
+    const postToXButton = document.getElementById("postToXButton");
 
-    document.getElementById("postToXButton").classList.remove("hidden");
+    // ボタンの初期設定
+    omikujiButton.disabled = true; // おみくじボタンを無効化
+    postToXButton.classList.add("hidden"); // ポストボタンを非表示にする
+
+    // スピン処理に必要な変数を作成
+    let index = 0; // 現在の公演情報を保持
+    let count = 0; // 現在の回転数を保持
+    let maxCount = 20; // 最大回転回数
+    let speed = 50; // 初期回転速度
+
+    function spin() {
+        if (count < maxCount) {
+            // おみくじの内容をランダムに変更
+            index = Math.floor(Math.random() * lectures.length);
+
+            // おみくじの内容を画面に表示
+            document.getElementById("omikujiTitle").textContent = lectures[index].title;
+            document.getElementById("omikujiDetails").textContent =
+                `${lectures[index].time}, ${lectures[index].company}, ホール: ${lectures[index].hall}`;
+
+            count++;
+
+            // 回転の速度を徐々に遅くする
+            speed += 7;
+            // seed[ms]後にspin関数を実行
+            setTimeout(spin, speed);
+        } else {
+            // 最終結果の公演情報を取得
+            const finalItem = lectures[index];
+            // 最終結果の表示
+            document.getElementById("omikujiTitle").textContent = finalItem.title;
+            document.getElementById("omikujiDetails").textContent =
+                `${finalItem.time}, ${finalItem.company}, ホール: ${finalItem.hall}`;
+
+            // ボタンを有効化/表示する設定
+            omikujiButton.disabled = false; // おみくじボタンを有効化
+            postToXButton.classList.remove("hidden"); // ポストボタンを表示
+        }
+    }
+
+    // スピン処理開始
+    spin();
 });
